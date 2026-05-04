dab604e055df94035f90d8ac8a0256044f119529
ziyad77 <Ziyaddev77@gmail.com>
2026-05-04 10:52:32 +0100

    add category page

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,9 +1,88 @@
-
+import { Button } from "../components/ui/button"
+import { Plus, CircleAlert } from "lucide-react"
 
 function Categories() {
   return (
-    <div className="">
-      <h1>categories</h1>
+    <div>
+
+      {/* header */}
+      <div className="flex items-center justify-between mb-7">
+        <div>
+          <h1 className="text-3xl font-semibold">Categories</h1>
+          <p className="text-sm text-gray-500">
+            Organize your spending with custom lables.
+          </p>
+        </div>
+        <Button onClick={() => setIsCreateExpenseModalOpen(true)}>
+          <Plus /> Add Expenses
+        </Button>
+      </div>
+      {/* === header === */}
+      {/* categries content */}
+      <div className="grid grid-cols-1 py-9 border-b border-gray-300/50 md:grid-cols-2 lg:grid-cols-3 gap-8 ">
+        <div className="ring ring-gray-300 rounded p-5 bg-white flex gap-3 items-start">
+          <span className="p-5 bg-gray-700 rounded inline-block"></span>
+          <div className="flex flex-col">
+            <span className="text-[#16332D] font-semibold">Rent</span>
+            <span className="text-[#16332D]/80 ">Monthly housing costs</span>
+          </div>
+        </div>
+          <div className="ring ring-gray-300 rounded p-5 bg-white flex gap-3 items-start">
+          <span className="p-5 bg-gray-700 rounded inline-block"></span>
+          <div className="flex flex-col">
+            <span className="text-[#16332D] font-semibold">Rent</span>
+            <span className="text-[#16332D]/80 ">Monthly housing costs</span>
+          </div>
+        </div>  <div className="ring ring-gray-300 rounded p-5 bg-white flex gap-3 items-start">
+          <span className="p-5 bg-gray-700 rounded inline-block"></span>
+          <div className="flex flex-col">
+            <span className="text-[#16332D] font-semibold">Rent</span>
+            <span className="text-[#16332D]/80 ">Monthly housing costs</span>
+          </div>
+        </div>  <div className="ring ring-gray-300 rounded p-5 bg-white flex gap-3 items-start">
+          <span className="p-5 bg-gray-700 rounded inline-block"></span>
+          <div className="flex flex-col">
+            <span className="text-[#16332D] font-semibold">Rent</span>
+            <span className="text-[#16332D]/80 ">Monthly housing costs</span>
+          </div>
+        </div>  <div className="ring ring-gray-300 rounded p-5 bg-white flex gap-3 items-start">
+          <span className="p-5 bg-gray-700 rounded inline-block"></span>
+          <div className="flex flex-col">
+            <span className="text-[#16332D] font-semibold">Rent</span>
+            <span className="text-[#16332D]/80 ">Monthly housing costs</span>
+          </div>
+        </div>  <div className="ring ring-gray-300 rounded p-5 bg-white flex gap-3 items-start">
+          <span className="p-5 bg-gray-700 rounded inline-block"></span>
+          <div className="flex flex-col">
+            <span className="text-[#16332D] font-semibold">Rent</span>
+            <span className="text-[#16332D]/80 ">Monthly housing costs</span>
+          </div>
+        </div>  <div className="ring ring-gray-300 rounded p-5 bg-white flex gap-3 items-start">
+          <span className="p-5 bg-gray-700 rounded inline-block"></span>
+          <div className="flex flex-col">
+            <span className="text-[#16332D] font-semibold">Rent</span>
+            <span className="text-[#16332D]/80 ">Monthly housing costs</span>
+          </div>
+        </div>  <div className="ring ring-gray-300 rounded p-5 bg-white flex gap-3 items-start">
+          <span className="p-5 bg-gray-700 rounded inline-block"></span>
+          <div className="flex flex-col">
+            <span className="text-[#16332D] font-semibold">Rent</span>
+            <span className="text-[#16332D]/80 ">Monthly housing costs</span>
+          </div>
+        </div>  <div className="ring ring-gray-300 rounded p-5 bg-white flex gap-3 items-start">
+          <span className="p-5 bg-gray-700 rounded inline-block"></span>
+          <div className="flex flex-col">
+            <span className="text-[#16332D] font-semibold">Rent</span>
+            <span className="text-[#16332D]/80 ">Monthly housing costs</span>
+          </div>
+        </div>
+      </div>
+      {/*=== categries content ===*/}
+      <div className="mt-9 text-[#16332D]/60">
+        <p className="flex items-center gap-2"><CircleAlert /> Catgories help the Budget Planner track your monthly spending limits more accurately.</p>
+      </div>
+
+
     </div>
   )
 }
